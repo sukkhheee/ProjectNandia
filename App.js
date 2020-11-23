0bd31e3d543cb4bbeb55485bb010a2063c5f0ebd
sukkhheee <73513818+sukkhheee@users.noreply.github.com>
2020-11-23 12:53:15 +0845

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {HeaderTop} from './HeaderTop.js'
+
+export function App(props) {
+  return (
+    <div>
+            <HeaderTop/>
+    </div>
+  );
+}
---
+++ HeaderTop.js
@@ -0,0 +1,39 @@
+import React from 'react';
+export function HeaderTop() {
+    return (
+        <div class="header-wrap active" id="header">
+        <div className="header">
+        <div className="container">
+            <div className="logo">
+                <a href="/https://monos.mn/mn">
+                    <img src="https://www.monos.mn/wp-content/themes/monos/img/logomn.png"/>
+                </a>
+            </div>
+            <div className="menu-wrap pull-right">
+            <ul id="menu-main-menu-mn-1" class="main-menu"><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-21"><a href="http://www.monos.mn/mn/бидний-тухай/бид/">Бидний тухай</a></li>
+                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-20"><a href="https://www.monos.mn/mn/%d2%af%d0%b9%d0%bb-%d0%b0%d0%b6%d0%b8%d0%bb%d0%bb%d0%b0%d0%b3%d0%b0%d0%b0/">Үйл ажиллагаа</a></li>
+                <li class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-85"><a href="https://www.monos.mn/mn/c/%d0%bc%d1%8d%d0%b4%d1%8d%d1%8d-%d0%bc%d1%8d%d0%b4%d1%8d%d1%8d%d0%bb%d1%8d%d0%bb/">Мэдээ</a></li>
+                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-19"><a href="http://www.monos.mn/mn/%D1%85%D2%AF%D0%BD%D0%B8%D0%B9-%D0%BD%D3%A9%D3%A9%D1%86/%D0%B1%D0%B8%D0%B4%D1%8D%D0%BD%D1%82%D1%8D%D0%B9-%D0%BD%D1%8D%D0%B3%D0%B4%D1%81%D1%8D%D0%BD%D1%8D%D1%8D%D1%80/">Хүний нөөц</a></li>
+                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2241"><a href="http://www.monospharma.mn/360_virtual/">360 Virtual tour</a></li>
+                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-23"><a href="https://www.emonos.mn/">Emonos.mn</a></li>
+            </ul>
+            <ul class="lang-menu">
+                <li class="menu-item"><a href="#_" data-toggle="modal" data-target="#searchModal"><i class="fa fa-search"></i></a></li>
+                <li class="menu-item"><a href="/en">en</a></li>
+                <li class="menu-item"><a href="/ru">ru</a></li>
+            </ul>
+
+            </div>
+
+        </div>
+      
+        </div>
+        <div className="content">
+            <div className="zurag">
+                <img src="../public/stat/images/animal_7.jpg"/>
+            </div>
+        </div>
+        </div>
+        
+    )
+}
